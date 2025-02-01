ac65f899f50f94882ff43a2a35f5a8a0cf832b0a
abdelhamid elbouz <abdelhamidelbouz@gmail.com>
2025-02-01 16:22:29 +0100

    Update creat_window.c

--- so_long/creat_window.c
+++ so_long/creat_window.c
@@ -6,19 +6,19 @@
 /*   By: aelbouz <aelbouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 10:48:11 by aelbouz           #+#    #+#             */
-/*   Updated: 2025/01/26 15:09:29 by aelbouz          ###   ########.fr       */
+/*   Updated: 2025/02/01 16:15:01 by aelbouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	get_map_size(char *filename, int *rows, int *cols)
+void	get_map_size(char *filename, int *rows, int *columns)
 {
 	int		fd;
 	char	*line;
 
 	*rows = lines_count(filename);
-	*cols = 0;
+	*columns = 0;
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 	{
@@ -28,62 +28,51 @@ void	get_map_size(char *filename, int *rows, int *cols)
 	line = get_next_line(fd);
 	if (line)
 	{
-		*cols = ft_strlen(line);
-		if (line[*cols - 1] == '\n')
-			(*cols)--;
+		*columns = ft_strlen(line);
+		if (line[*columns - 1] == '\n')
+			(*columns)--;
 		free(line);
 	}
-	close (fd);
+	close(fd);
 }
 
-void	img_to_window(t_game *game, char **map, int rows, int cols)
+void	img_to_window(t_game *game, char **map)
 {
 	int	x;
 	int	y;
 
 	y = 0;
-	ft_printf("rows:%d\ncols:%d\n", rows, cols);
-	if (!map) // Check if map is NULL
+	while (y < game->rows)
 	{
-		ft_printf("Error: map is NULL\n");
-		return;
-	}
-	while (y < rows)
-	{
-		if (!map[y]) // Check if map[y] is NULL
-		{
-			ft_printf("Error: map[%d][%d] is NULL\n", y, x);
-			break;
-		}
 		x = 0;
-		while (x < cols)
+		while (x < game->columns)
 		{
 			ft_printf("x:%d\ny:%d\n", x, y);
 			if (map[y][x] == '1')
-			{
-				mlx_put_image_to_window(game->mlx, game->win, game->w_img, x * 128, y * 128);
-			}
+				placing(game, y, x, game->w_img);
 			if (map[y][x] == 'E')
-				mlx_put_image_to_window(game->mlx, game->win, game->e_img, x * 128, y * 128);
+				placing(game, y, x, game->e_img);
 			if (map[y][x] == 'P')
-				mlx_put_image_to_window(game->mlx, game->win, game->p_img, x * 128, y * 128);
+				placing(game, y, x, game->p_img);
 			if (map[y][x] == 'C')
-				mlx_put_image_to_window(game->mlx, game->win, game->c_img, x * 128, y * 128);
+				placing(game, y, x, game->c_img);
+			if (map[y][x] == '0')
+				placing(game, y, x, game->s_img);
 			x++;
 		}
 		y++;
 	}
 }
 
-void	creat_window(t_data *data, int rows, int cols)
+void	creat_window(t_game *game, int rows, int columns)
 {
-	data->mlx = mlx_init();
-	data->win = mlx_new_window(data->mlx, rows * 128, cols * 128, "so_long");
-	if (!data->win)
+	game->mlx = mlx_init();
+	game->win = mlx_new_window(game->mlx, rows * 64, columns * 64, "so_long");
+	if (!game->win)
 	{
-		free(data->win);
+		free(game->win);
 		exit(0);
 	}
-	mlx_key_hook(data->win, handl_esc_key, data);
-	mlx_hook(data->win, 17, 0, handl_red_cross, data);
+	mlx_key_hook(game->win, handl_esc_key, game);
+	mlx_hook(game->win, 17, 0, handl_red_cross, game);
 }
