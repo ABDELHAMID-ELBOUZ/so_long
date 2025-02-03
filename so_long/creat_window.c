7844bb7e75bbc925033a840f5720d8f90354c104
abdelhamid elbouz <abdelhamidelbouz@gmail.com>
2025-02-03 10:06:47 +0100

    Update creat_window.c

--- so_long/creat_window.c
+++ so_long/creat_window.c
@@ -6,7 +6,7 @@
 /*   By: aelbouz <aelbouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 10:48:11 by aelbouz           #+#    #+#             */
-/*   Updated: 2025/02/01 16:15:01 by aelbouz          ###   ########.fr       */
+/*   Updated: 2025/02/03 09:57:45 by aelbouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,16 +47,15 @@ void	img_to_window(t_game *game, char **map)
 		x = 0;
 		while (x < game->columns)
 		{
-			ft_printf("x:%d\ny:%d\n", x, y);
 			if (map[y][x] == '1')
 				placing(game, y, x, game->w_img);
-			if (map[y][x] == 'E')
+			else if (map[y][x] == 'E')
 				placing(game, y, x, game->e_img);
-			if (map[y][x] == 'P')
+			else if (map[y][x] == 'P')
 				placing(game, y, x, game->p_img);
-			if (map[y][x] == 'C')
+			else if (map[y][x] == 'C')
 				placing(game, y, x, game->c_img);
-			if (map[y][x] == '0')
+			else if (map[y][x] == '0')
 				placing(game, y, x, game->s_img);
 			x++;
 		}
@@ -64,10 +63,10 @@ void	img_to_window(t_game *game, char **map)
 	}
 }
 
-void	creat_window(t_game *game, int rows, int columns)
+void	creat_window(t_game *game, int columns, int rows)
 {
 	game->mlx = mlx_init();
-	game->win = mlx_new_window(game->mlx, rows * 64, columns * 64, "so_long");
+	game->win = mlx_new_window(game->mlx, columns * 64, rows * 64, "so_long");
 	if (!game->win)
 	{
 		free(game->win);
