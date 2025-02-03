f93692c5acc3914a835b67505f8b8e3a7da18ae5
abdelhamid elbouz <abdelhamidelbouz@gmail.com>
2025-02-03 10:09:02 +0100

    Update image.c

--- so_long/image.c
+++ so_long/image.c
@@ -6,31 +6,30 @@
 /*   By: aelbouz <aelbouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 14:58:38 by aelbouz           #+#    #+#             */
-/*   Updated: 2025/02/01 16:06:21 by aelbouz          ###   ########.fr       */
+/*   Updated: 2025/02/02 18:30:40 by aelbouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	file_to_image(t_game *game, char **map)
+int	file_to_image(t_game *game)
 {
 	int	i;
 	int	j;
 
 	game->w_img = mlx_xpm_file_to_image(game->mlx, "./images/wall.xpm", &i, &j);
-	game->s_img = mlx_xpm_file_to_image(game->mlx, "./images/spac.xpm", &i, &j);
+	game->s_img = mlx_xpm_file_to_image(game->mlx, "./images/road.xpm", &i, &j);
 	game->c_img = mlx_xpm_file_to_image(game->mlx, "./images/coin.xpm", &i, &j);
 	game->p_img = mlx_xpm_file_to_image(game->mlx, "./images/char.xpm", &i, &j);
 	game->e_img = mlx_xpm_file_to_image(game->mlx, "./images/exit.xpm", &i, &j);
 	if (!game->e_img || !game->w_img || !game->s_img \
 		|| !game->c_img || !game->p_img)
 		return (ft_printf("Error\neroor images\n"), exit(0), 0);
-	img_to_window(game, map);
 	return (1);
 }
 
-void	placing(t_game *game, int rows, int columns, void *which)
+void	placing(t_game *game, int y, int x, void *which)
 {
 	mlx_put_image_to_window(game->mlx, game->win, which, \
-		rows * 64, columns * 64);
+		x * 64, y * 64);
 }
